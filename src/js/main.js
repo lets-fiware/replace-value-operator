67780969fb1ab075ff03b1e9babcee321e38c4ab
Kazuhito Suda <kazuhito@fisuda.jp>
2019-11-23 15:43:36 +0900

    first commit

---
+++ src/js/main.js
@@ -0,0 +1,48 @@
+/*
+ * replace-value
+ * https://github.com/lets-fiware/replace-value-operator
+ *
+ * Copyright (c) 2019 Kazuhito Suda
+ * Licensed under the MIT license.
+ */
+
+(function () {
+
+    "use strict";
+
+    var getReplace = function getReplace() {
+        var array = {};
+        var values = MashupPlatform.prefs.get("replace").trim().split(',');
+        if (values.length % 2 == 0) {
+            for (var i = 0; i < values.length / 2; i++) {
+                array[values[i]] = values[i + 1];
+            }
+        } else {
+            throw new MashupPlatform.wiring.EndpointValueError();
+        }
+        return array;
+    }
+
+    var replaceValue = function replaceValue(value) {
+        var replace = getReplace();
+        if (value != null) {
+            value = replace[value] || value;
+            MashupPlatform.wiring.pushEvent("output", value);
+
+        } else if (value == null && MashupPlatform.prefs.get("send_nulls")) {
+            MashupPlatform.wiring.pushEvent("output", value);
+        }
+    }
+
+    /* TODO
+     * this if is required for testing, but we have to search a cleaner way
+     */
+    if (window.MashupPlatform != null) {
+        MashupPlatform.wiring.registerCallback("input", replaceValue);
+    }
+
+    /* test-code */
+    window.replaceValue = replaceValue;
+    /* end-test-code */
+
+})();
---
+++ tests/js/ReplaceValueSpec.js
@@ -0,0 +1,86 @@
+/*
+ * replace-value-list
+ * https://github.com/lets-fiware/replace-value-list-operator
+ *
+ * Copyright (c) 2019 Kazuhito Suda
+ * Licensed under the MIT license.
+ */
+
+/* globals MockMP */
+
+(function () {
+
+    "use strict";
+
+    describe("ReplaceValueList", function () {
+
+        beforeAll(function () {
+            window.MashupPlatform = new MockMP({
+                type: 'operator',
+                prefs: {
+                    "send_nulls": true,
+                    "replace": "-9999,missing data"
+                },
+                inputs: ['input'],
+                outputs: ['output']
+            });
+        });
+
+        beforeEach(function () {
+            MashupPlatform.reset();
+            MashupPlatform.operator.outputs.output.connect({simulate: () => {}});
+        });
+
+        it("no replace (numeric)", function () {
+            replaceValue(1);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', 1);
+        });
+
+        it("no replace (string)", function () {
+            replaceValue("abc");
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', "abc");
+        });
+
+        it("replace (numeric)", function () {
+            replaceValue(-9999);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', "missing data");
+        });
+
+        it("replace (string)", function () {
+            MashupPlatform.prefs.set("replace", "abc,xyz");
+
+            replaceValue("abc");
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', "xyz");
+        });
+
+        it("allowed to send nulls", function () {
+            MashupPlatform.prefs.set("send_nulls", true);
+
+            replaceValue(null);
+
+            expect(MashupPlatform.wiring.pushEvent).toHaveBeenCalledWith('output', null);
+        });
+
+        it("disallowed to send nulls", function () {
+            MashupPlatform.prefs.set("send_nulls", false);
+
+            replaceValue(null);
+
+            expect(MashupPlatform.wiring.pushEvent).not.toHaveBeenCalled();
+        });
+
+        it("replace parameter error", function () {
+            MashupPlatform.prefs.set("replace", "123,abc,xyz");
+
+            expect(function () {
+                replaceValue(5);
+            }).toThrowError(MashupPlatform.wiring.EndpointValueError);
+        });
+
+
+    });
+})();
