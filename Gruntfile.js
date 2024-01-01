1a2160582f191f4409e3eaecafe929ad6573600d
Kazuhito Suda <kazuhito@fisuda.jp>
2024-01-01 16:06:13 +0900

    Update copyright date

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
  * replace-value
  * https://github.com/lets-fiware/replace-value-operator
  *
- * Copyright (c) 2019-2023 Kazuhito Suda
+ * Copyright (c) 2019-2024 Kazuhito Suda
  * Licensed under the MIT license.
  */
 
